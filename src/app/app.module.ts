1e7c0728b738415b0247e89762a4c1336a6a5074
talshbt <talshbt@gmail.com>
2020-03-30 09:54:27 +0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ModalComponent } from './modal/modal.component';
-import { NgbdModalComponent } from './ngbd-modal/ngbd-modal.component';
-import { NgbdModalContentComponent } from './ngbd-modal-content/ngbd-modal-content.component';
+
 import { NgModalComponent } from './ng-modal/ng-modal.component';
 
 @NgModule({
-  imports: [BrowserModule, NgbModule],
-
-  declarations: [ AppComponent, HelloComponent, ModalComponent, NgbdModalComponent, NgbdModalContentComponent, NgModalComponent ],
-    exports: [NgbdModalComponent],
-
-  bootstrap:    [ AppComponent ],
-    entryComponents: [NgbdModalContentComponent]
+ imports: [
+    BrowserModule,
+    FormsModule,
+    NgbModalModule
+  ],
+  declarations: [
+    AppComponent,
+    NgModalComponent
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [NgModalComponent]
 
 })
 export class AppModule { }
