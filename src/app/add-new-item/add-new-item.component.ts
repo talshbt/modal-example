c46db8e6f661983aca3ac4a1114d5dd4b20013b5
talshbt <talshbt@gmail.com>
2020-03-30 14:17:03 +0300

    4 files modified

--- src/app/add-new-item/add-new-item.component.ts
+++ src/app/add-new-item/add-new-item.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Subscription } from "rxjs";
+import {TableService} from '../shared/table.service'
 
 @Component({
   selector: 'app-add-new-item',
@@ -7,9 +10,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNewItemComponent implements OnInit {
 
-  constructor() { }
+   @ViewChild("f", { static: false }) signupForm: NgForm;
+    cols = [];
+  // private fieldArray = ["id","name","email"];
+
+  constructor( private tableService: TableService) { 
+    
+  }
 
   ngOnInit() {
+     this.cols = this.tableService.getCols();
+  }
+
+    private getFormDetails() {
+      this.cols.forEach(function (data) {
+        console.log(data);
+      
+    }); 
+    
+   
+
+  }
+
+  onSubmit() {
+    
+    // let data:String = this.tableService.getCols()[0];
+    // let cols = this.tableService.getCols();
+    let rowDetails = [];
+    let rowDetailsObj = {};
+      for(var i = 0 ; i < this.cols.length; ++i){
+        rowDetails.push(this.signupForm.value[this.cols[i]]);
+
+        // console.log(this.signupForm.value[this.cols[i]]);
+      }
+
+
+       rowDetailsObj = this.createObj();
+       console.log(rowDetailsObj);
+
+      this.tableService.addRow(rowDetailsObj);
+      this.signupForm.reset();
+      this.context.resolve();
+
+  }
+
+
+  createObj(){
+    console.log("createObj")
+    var rowDetails = { };
+      for(var i = 0 ; i < this.cols.length; ++i){
+        // rowDetails.push(this.signupForm.value[this.cols[i]]);
+        rowDetails[this.cols[i]] = this.signupForm.value[this.cols[i]];
+      }
+
+      // console.log(rowDetails);
+      return rowDetails;
+
+
+
   }
 
 }
