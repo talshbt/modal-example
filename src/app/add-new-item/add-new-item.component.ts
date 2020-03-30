10902f4b769440cd6ff254fab1b1df8c648c5558
talshbt <talshbt@gmail.com>
2020-03-30 14:14:46 +0300

    2 files modified, 1 added, 1 deleted

--- src/app/add-new-item/add-new-item.component.ts
+++ src/app/add-new-item/add-new-item.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-new-item',
   templateUrl: './add-new-item.component.html',
-  styleUrls: ['./add-new-item.component.css']
+  styleUrls: ['./add-new-item.component.scss']
 })
 export class AddNewItemComponent implements OnInit {
 
