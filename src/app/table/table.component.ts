dfff1c5155ff2f857ecbe127805ac489060c254a
talshbt <talshbt@gmail.com>
2020-03-30 13:55:39 +0300

    1 file modified, 2 added, 1 deleted

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, OnInit } from '@angular/core';
+import {TableService} from '../shared/table.service'
+import { AddNewItemComponent } from '../add-new-item/add-new-item.component';
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
+  sub: Subscription;
+  cols = [];
+  rowDetailsArr = [];
 
-  constructor() { }
+  constructor(private tableService: TableService) { }
 
   ngOnInit() {
+     this.sub = this.tableService.rowAdded.subscribe(
+       (rowDetailsArr) => {
+        this.rowDetailsArr = rowDetailsArr;
+        console.log("table get new row ");
+        console.log(this.rowDetailsArr);
+
+      }
+     )
+
+     this.cols = this.tableService.getCols();
+     console.log(this.cols);
+  }
+
+  async onAdd() {
+    // this.tableService.getModalEvent();
+    // console.log(await this.modalService.open(AddNewItemComponent));
   }
 
 }
