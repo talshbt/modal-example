3f5e9612208200df878372843de61dbe1bef055d
talshbt <talshbt@gmail.com>
2020-03-30 09:20:21 +0300

    2 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ModalComponent } from './modal/modal.component';
+import { NgbdModalComponent } from './ngbd-modal/ngbd-modal.component';
+import { NgbdModalContentComponent } from './ngbd-modal-content/ngbd-modal-content.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ModalComponent, NgbdModalComponent, NgbdModalContentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/ngbd-modal-content/ngbd-modal-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngbd-modal-content',
+  templateUrl: './ngbd-modal-content.component.html',
+  styleUrls: ['./ngbd-modal-content.component.css']
+})
+export class NgbdModalContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
