d4230675485979086877056525d0ff83da61de95
talshbt <talshbt@gmail.com>
2020-04-02 14:14:39 +0300

    1 file modified

--- src/app/shared/table.service.ts
+++ src/app/shared/table.service.ts
@@ -1,88 +1,68 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
-
 
 @Injectable()
 export class TableService {
   modalOpened = new Subject<any>();
   rowChanged = new Subject<any>();
   saveData = new Subject<any>();
-  rowEdit = new Subject<any>();
   rowToEdit = [];
   onEditMode = false;
   rowIndexToEdit = null;
-  private cols = ["id","name","email"];
+  private cols = ["id", "name", "email"];
   private rowsDetailsArr = [];
-
-  
 
   private rowsDetailsObj = {};
 
-  constructor() { }
+  constructor() {}
 
-  getModalEvent(){
+  getModalEvent() {
     this.modalOpened.next();
   }
 
-  addRow(row){
-      if (this.onEditMode){
-          this.rowsDetailsArr[this.rowIndexToEdit] = row;
-          this.onEditMode = false;
-      }else{
-        this.rowsDetailsArr.push(row);
-      }
-      
-      this.rowChanged.next(this.rowsDetailsArr.slice());
+  addRow(row) {
+    if (this.onEditMode) {
+      this.rowsDetailsArr[this.rowIndexToEdit] = row;
+      this.onEditMode = false;
+    } else {
+      this.rowsDetailsArr.push(row);
+    }
 
-
+    this.rowChanged.next(this.rowsDetailsArr.slice());
   }
 
-  getCols(){
+  getCols() {
     return this.cols;
   }
 
-
-
-  onSaveData(){
-    console.log(this.onEditMode)
+  onSaveData() {
+    console.log(this.onEditMode);
     this.saveData.next("save data in service");
   }
 
-  deleteRow(indexRow){
-   this.rowsDetailsArr.splice(indexRow,1);
-   this.rowChanged.next(this.rowsDetailsArr.slice())
-
+  deleteRow(indexRow) {
+    this.rowsDetailsArr.splice(indexRow, 1);
+    this.rowChanged.next(this.rowsDetailsArr.slice());
   }
 
-  editRow(indexRow){
+  editRow(indexRow) {
     this.onEditMode = true;
     this.rowIndexToEdit = indexRow;
-    var rowDetails =  []
-     for(var i = 0 ; i < this.cols.length; ++i){
-        this.rowToEdit.push(this.rowsDetailsArr[indexRow][this.cols[i]]);
-     }
-
-
-
+    var rowDetails = [];
+    for (var i = 0; i < this.cols.length; ++i) {
+      this.rowToEdit.push(this.rowsDetailsArr[indexRow][this.cols[i]]);
+    }
   }
 
-
-  getRowToEdit(){
-
-    if(this.onEditMode){
-       return this.rowToEdit;
-    }else{
+  getRowToEdit() {
+    if (this.onEditMode) {
+      return this.rowToEdit;
+    } else {
       return [];
     }
-    
-   
   }
 
-
-
-  isEditMode(){
+  isEditMode() {
     return this.onEditMode;
-
   }
-
 }
