eda4d54b2e056b6981057dca46aa2a9954a18961
talshbt <talshbt@gmail.com>
2020-04-05 13:50:44 +0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,33 +10,32 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 export class AppComponent {
   constructor(
-    private _NgbModal: NgbModal
   ) { }
 
   // open modal
-  openModal() {
-    this._NgbModal.open(NgModalComponent, {
-      windowClass: 'modal-job-scrollable'
-    });
+  // openModal() {
+  //   this._NgbModal.open(NgModalComponent, {
+  //     windowClass: 'modal-job-scrollable'
+  //   });
 
-    // upwrap the "app-ng-modal" data to enable the "modal-dialog-scrollable"
-    // and make the modal scrollable
-    (() => {
-      const node: HTMLElement | null = document.querySelector('app-ng-modal');
-      if (node) {
-        while (node.firstChild) {
-          (node.parentNode as HTMLElement).insertBefore(node.firstChild, node);
-        }
-      }
-      // make the modal scrollable by adding the class .modal-dialog-scrollable
-      // here wait for one second so that we can find the .modal-dialog
-      setTimeout(() => {
-        const modalDialog = document.querySelector('.modal-job-scrollable .modal-dialog');
-        if (modalDialog) {
-          modalDialog.classList.add('modal-dialog-scrollable');
-        }
-      }, 1000)
-    })();
-  }
+  //   // upwrap the "app-ng-modal" data to enable the "modal-dialog-scrollable"
+  //   // and make the modal scrollable
+  //   (() => {
+  //     const node: HTMLElement | null = document.querySelector('app-ng-modal');
+  //     if (node) {
+  //       while (node.firstChild) {
+  //         (node.parentNode as HTMLElement).insertBefore(node.firstChild, node);
+  //       }
+  //     }
+  //     // make the modal scrollable by adding the class .modal-dialog-scrollable
+  //     // here wait for one second so that we can find the .modal-dialog
+  //     setTimeout(() => {
+  //       const modalDialog = document.querySelector('.modal-job-scrollable .modal-dialog');
+  //       if (modalDialog) {
+  //         modalDialog.classList.add('modal-dialog-scrollable');
+  //       }
+  //     }, 1000)
+  //   })();
+  // }
 
 }
